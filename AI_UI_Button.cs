65fd2bc4cafb5ce379a8eb9cd6a791fb579faf3b
yhmu <41369531+yhmu@users.noreply.github.com>
2018-10-06 02:33:18 +1000

    Add files via upload

---
+++ AI_UI_Button.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AI_UI_Button : MonoBehaviour
+{
+
+    public void BackMenu()
+    {
+        SceneManager.LoadScene("UIDesgin");
+    }
+
+    public void QuitAll()
+    {
+        Debug.Log("Quit!!!!!");
+        Application.Quit();
+    }
+}
+
---
+++ screen_resolution.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class screen_resolution : MonoBehaviour {
+
+    public bool isScalableX;
+    public bool isScalableY;
+    public bool isRepositionableX;
+    public bool isRepositionableY;
+    public float ratio = 1.76f;
+
+    void Start()
+    {
+        float scaleFactor = Camera.main.aspect / ratio;
+
+        if (isScalableX)
+        {
+            this.gameObject.transform.localScale = new Vector3(this.gameObject.transform.localScale.x * scaleFactor, this.gameObject.transform.localScale.y, this.gameObject.transform.localScale.z);
+        }
+        if (isScalableY)
+        {
+            this.gameObject.transform.localScale = new Vector3(this.gameObject.transform.localScale.x, this.gameObject.transform.localScale.y * scaleFactor, this.gameObject.transform.localScale.z);
+        }
+
+        if (isRepositionableX)
+        {
+            if (isRepositionableY)
+                this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x * scaleFactor, this.gameObject.transform.position.y * scaleFactor, this.gameObject.transform.position.z);
+            else
+                this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x * scaleFactor, this.gameObject.transform.position.y, this.gameObject.transform.position.z);
+        }
+    }
+}
